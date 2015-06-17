29ee65bb9d71c887bd7dc51437b98957229e16a3
fnavarro92 <franciscojavier.navarroleblic@gmail.com>
2015-06-17 20:46:09 +0200

    tarara

--- EuloWarBean/src/java/webservices/OperacionesCombates.java
+++ EuloWarBean/src/java/webservices/OperacionesCombates.java
@@ -33,24 +33,24 @@ import session.UsuarioFacade;
 @WebService(serviceName = "OperacionesCombates")
 @Stateless()
 public class OperacionesCombates {
-    
+
     @EJB
     private FabricaDefensivaFacade fabricaDefensivaFacade;
     @EJB
     private FabricaOfensivaFacade fabricaOfensivaFacade;
     @EJB
     private UsuarioFacade usuarioFacade;
-    @EJB 
+    @EJB
     private TropaAtaqueFacade tropaAtaqueFacade;
     @EJB
     private TropaDefensaFacade tropaDefensaFacade;
-    @EJB 
+    @EJB
     private NivelTropasOfensivasFacade nivelTropasOfensivasFacade;
-    @EJB 
+    @EJB
     private NivelTropasDefensivasFacade nivelTropasDefensivasFacade;
-    
+
     private Random random = new Random();
-    
+
     private final String A_TANQUE = "TANQUE";
     private final int FUERZA_A_TANQUE = 20;
     private final String A_PELOTON = "PELOTON";
@@ -63,7 +63,7 @@ public class OperacionesCombates {
     private final int FUERZA_A_HELICOPTERO = 40;
     private final String A_CHUCK_NORRIS = "CHUCK_NORRIS";
     private final int FUERZA_A_CHUNK_NORRIS = 160;
-    
+
     private final String D_MISIL_ANTIAEREO = "MISIL_ANTIAEREO";
     private final int FUERZA_D_MISIL_ANTIAEREO = 80;
     private final String D_TRINCHERA_AMETRALLADORAS = "TRINCHERA_AMETRALLADORAS";
@@ -76,52 +76,54 @@ public class OperacionesCombates {
     private final int FUERZA_D_CANYON_COMBATE = 20;
     private final String D_CANYON_ANTIAEREO = "CAÑON_ANTIAEREO";
     private final int FUERZA_D_CANYON_ANTIAEREO = 40;
-    
-    
 
     /**
-     * Elige un rival para un ataque en funcion de los niveles de fabrica ofensiva
-     * y defensiva.
+     * Elige un rival para un ataque en funcion de los niveles de fabrica
+     * ofensiva y defensiva.
+     *
      * @param email
-     * @return 
+     * @return
      */
     @WebMethod(operationName = "elegirRival")
     public Usuario elegirRival(@WebParam(name = "email") String email) {
         //TODO write your implementation code here:
         FabricaOfensiva fabricaOfensivaUsuario;
-        FabricaDefensiva fabricaRival;
-                
+        FabricaDefensiva fabricaRival = null;
+
         fabricaOfensivaUsuario = fabricaOfensivaFacade.obtenerFabricaFromEmail(email);
         System.out.println("FABRICAOFENSIVA::::" + fabricaOfensivaUsuario);
         int nivelFabrica = fabricaOfensivaUsuario.getNivelFabricaOfensiva();
-        
+
         List<FabricaDefensiva> listaFabricasRivales = fabricaDefensivaFacade.findAll();
         List<FabricaDefensiva> listaFabricasMismoNivel = new ArrayList<>();
-        for (FabricaDefensiva fabricaDefensivaRival : listaFabricasRivales){
-            if (nivelFabrica == fabricaDefensivaRival.getNivelFabricaDefensiva()){
+        for (FabricaDefensiva fabricaDefensivaRival : listaFabricasRivales) {
+            if (nivelFabrica == fabricaDefensivaRival.getNivelFabricaDefensiva()) {
                 listaFabricasMismoNivel.add(fabricaDefensivaRival);
             }
         }
-        if (listaFabricasMismoNivel.isEmpty()){
-            //ELEGIR UN RIVAL CUALQUIERA.
-            int aleatorio = random.nextInt(listaFabricasRivales.size());
-            fabricaRival = listaFabricasRivales.get(aleatorio);
-            if (!fabricaRival.getFKFabricaDefensivaUsuario().getEmail().equals(email)){
-                System.out.println("RIVALISIMO ES: "+ fabricaRival.getFKFabricaDefensivaUsuario().getEmail());
-                return fabricaRival.getFKFabricaDefensivaUsuario();
-                
+        if (listaFabricasMismoNivel.isEmpty()) {
+            while (fabricaRival == null) {
+                //ELEGIR UN RIVAL CUALQUIERA.
+                int aleatorio = random.nextInt(listaFabricasRivales.size());
+                fabricaRival = listaFabricasRivales.get(aleatorio);
+                if (!fabricaRival.getFKFabricaDefensivaUsuario().getEmail().equals(email)) {
+                    System.out.println("RIVALISIMO ES: " + fabricaRival.getFKFabricaDefensivaUsuario().getEmail());
+                    return fabricaRival.getFKFabricaDefensivaUsuario();
+
+                }
             }
         } else {
             //ELEGIR UN RIVAL CUALQUIERA DE ESA LISTA.
-            int aleatorioNivel = random.nextInt(listaFabricasMismoNivel.size());
-            fabricaRival = listaFabricasMismoNivel.get(aleatorioNivel);
-            if (!fabricaRival.getFKFabricaDefensivaUsuario().getEmail().equals(email)){
-                System.out.println("RIVALISIMO ES: "+ fabricaRival.getFKFabricaDefensivaUsuario().getEmail());
-                return fabricaRival.getFKFabricaDefensivaUsuario();
+            while (fabricaRival == null) {
+                int aleatorioNivel = random.nextInt(listaFabricasMismoNivel.size());
+                fabricaRival = listaFabricasMismoNivel.get(aleatorioNivel);
+                if (!fabricaRival.getFKFabricaDefensivaUsuario().getEmail().equals(email)) {
+                    System.out.println("RIVALISIMO ES: " + fabricaRival.getFKFabricaDefensivaUsuario().getEmail());
+                    return fabricaRival.getFKFabricaDefensivaUsuario();
+                }
             }
         }
         return null;
-        
 
     }
 
@@ -131,87 +133,112 @@ public class OperacionesCombates {
     @WebMethod(operationName = "simularCombate")
     public Usuario simularCombate(@WebParam(name = "emailAtacante") String emailAtacante, @WebParam(name = "emailDefensor") String emailDefensor) {
         //TODO write your implementation code here:
+        System.out.println("PRINCIPIO");
         int fuerzaAtaqueRival = 0;
         int fuerzaDefensaRival = 0;
         Usuario ganador = null;
-        
+
+        System.out.println("PUNTO 2");
         List<TropaDefensa> tropasDefensivas = tropaDefensaFacade.obtenerTropasFromEmail(emailDefensor);
         List<TropaAtaque> tropasOfensivas = tropaAtaqueFacade.obtenerTropasFromEmail(emailAtacante);
-        
+
+        System.out.println("PUNTO 3");
+
         List<String> tiposOfensivos = nivelTropasOfensivasFacade.getNombresTropas();
         List<String> tiposDefensivos = nivelTropasOfensivasFacade.getNombresTropas();
+        System.out.println("PUNTO 4");
 
-        for (TropaAtaque tropaAtacante : tropasOfensivas){
+        for (TropaAtaque tropaAtacante : tropasOfensivas) {
             //MULTIPLICAR FUERZA * UNIDADES * NIVEL
-            switch (tropaAtacante.getTipoTropaAtaque()){
+            System.out.println("DENTRO DEL FOR");
+            switch (tropaAtacante.getTipoTropaAtaque()) {
                 case A_AVION_COMBATE:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_AVION_COMBATE;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_AVION_COMBATE;
                     break;
                 case A_CHUCK_NORRIS:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_CHUNK_NORRIS;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_CHUNK_NORRIS;
                     break;
                 case A_HELICOPTERO:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_HELICOPTERO;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_HELICOPTERO;
                     break;
                 case A_PELOTON:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_PELOTON;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_PELOTON;
                     break;
                 case A_TANQUE:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_TANQUE;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_TANQUE;
                     break;
                 case A_VEHICULO_BLINDADO:
-                    fuerzaAtaqueRival += tropaAtacante.getUnidades()*FUERZA_A_VEHICULO_BLINDADO;
+                    fuerzaAtaqueRival += tropaAtacante.getUnidades() * FUERZA_A_VEHICULO_BLINDADO;
                     break;
                 default:
                     break;
             }
         }
-        for (TropaDefensa tropaDefensora : tropasDefensivas){
-            switch (tropaDefensora.getTipoTropaDefensa()){
+        for (TropaDefensa tropaDefensora : tropasDefensivas) {
+            System.out.println("DENTRO DEL SEGUNDO FOR");
+            switch (tropaDefensora.getTipoTropaDefensa()) {
                 case D_CAMPO_MINAS:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_CAMPO_MINAS;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_CAMPO_MINAS;
                     break;
                 case D_CANYON_ANTIAEREO:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_CANYON_ANTIAEREO;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_CANYON_ANTIAEREO;
                     break;
                 case D_CANYON_COMBATE:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_CANYON_COMBATE;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_CANYON_COMBATE;
                     break;
                 case D_MISIL_ANTIAEREO:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_MISIL_ANTIAEREO;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_MISIL_ANTIAEREO;
                     break;
                 case D_RAMBO:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_RAMBO;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_RAMBO;
                     break;
                 case D_TRINCHERA_AMETRALLADORAS:
-                    fuerzaDefensaRival += tropaDefensora.getUnidades()*FUERZA_D_TRINCHERA_AMETRALLADORAS;
+                    fuerzaDefensaRival += tropaDefensora.getUnidades() * FUERZA_D_TRINCHERA_AMETRALLADORAS;
                     break;
                 default:
                     break;
             }
         }
-        
+
+        System.out.println("FUERA DE LOS FOR");
+
         double fuerzaAtaqueDefinitiva = 0.0;
         double fuerzaDefensaDefinitiva = 0.0;
-        do{
+
+        do {
+            System.out.println("DO_WHILE");
             double numeroAleatorioAtaque = random.nextDouble();
             double numeroAleatorioDefensa = random.nextDouble();
-        
-            fuerzaAtaqueDefinitiva = numeroAleatorioAtaque*fuerzaAtaqueRival;
-            fuerzaDefensaDefinitiva = numeroAleatorioDefensa*fuerzaDefensaRival;
-            
-            if (fuerzaAtaqueDefinitiva > fuerzaDefensaDefinitiva){
+
+            if (fuerzaAtaqueRival == 0) {
+                fuerzaAtaqueRival = 1;
+            }
+
+            if (fuerzaDefensaRival == 0) {
+                fuerzaDefensaRival = 1;
+            }
+
+            fuerzaAtaqueDefinitiva = numeroAleatorioAtaque * fuerzaAtaqueRival;
+            fuerzaDefensaDefinitiva = numeroAleatorioDefensa * fuerzaDefensaRival;
+
+            System.out.println("FUERZA SIN ALEATORIO DE ARAQUE: " + fuerzaAtaqueRival);
+            System.out.println("FUERZA SIN ALEATORIO DE DEFENSA: " + fuerzaDefensaRival);
+
+            System.out.println(fuerzaAtaqueDefinitiva);
+            System.out.println(fuerzaDefensaDefinitiva);
+
+            if (fuerzaAtaqueDefinitiva > fuerzaDefensaDefinitiva) {
                 ganador = usuarioFacade.findByEmail(emailAtacante);
             } else {
                 ganador = usuarioFacade.findByEmail(emailDefensor);
             }
-        }while(fuerzaAtaqueDefinitiva == fuerzaDefensaDefinitiva);
+        } while (fuerzaAtaqueDefinitiva == fuerzaDefensaDefinitiva);
+
         System.out.println("FUERZA SIN ALEATORIO DE ARAQUE: " + fuerzaAtaqueRival);
         System.out.println("FUERZA SIN ALEATORIO DE DEFENSA: " + fuerzaDefensaRival);
-        
+
         System.out.println("GANADORRRRRRRR: " + ganador.getEmail());
         return ganador;
     }
 
-    
 }
